4f8ee475dd28acc0fc4d5b67acb9fa8cfcc310b6
Balancer <balancer@balancer.ru>
2010-06-28 07:51:12 +0400

    Показ неоднозначных оценок на http://balancer.ru/tools/votes/

--- classes/bors/votes/last.php
+++ classes/bors/votes/last.php
@@ -16,16 +16,34 @@ class bors_votes_last extends base_page
 		$best = objects_array('bors_votes_thumb', array(
 				'group' => 'target_class_name,target_object_id',
 				'order' => 'SUM(score) DESC',
-				'limit' => 20,
+				'limit' => 50,
+		));
+
+		$best_of_month = objects_array('bors_votes_thumb', array(
+				'create_time>' => time()-86400*30,
+				'group' => 'target_class_name,target_object_id',
+				'order' => 'SUM(score) DESC',
+				'limit' => 50,
+		));
+
+		$differents = objects_array('bors_votes_thumb', array(
+				'create_time>' => time()-86400*30,
+				'group' => 'target_class_name,target_object_id',
+				'order' => 'SUM(ABS(score))/(SUM(score) + 1) DESC',
+				'limit' => 10,
 		));
 
 		bors_objects_targets_preload($last);
 		bors_objects_targets_preload($best);
+		bors_objects_targets_preload($best_of_month);
+		bors_objects_targets_preload($differents);
 
 		return array(
 			'last' => $last,
 			'total' => objects_count('bors_votes_thumb', array()),
 			'best' => $best,
+			'best_of_month' => $best_of_month,
+			'differents' => $differents,
 		);
 	}
 
--- classes/bors/votes/last.tpl.php
+++ classes/bors/votes/last.tpl.php
@@ -1,9 +1,31 @@
+<table class="null"><tr><td>
 <dl class="box">
-<dt>Лучшие <?=count($best)?> сообщений форума</dt>
+<dt>Лучшие <?=count($best)?> сообщений форума за месяц</dt>
+<dd>
+<?php foreach($best_of_month as $x) if($x && $x->target()):?>
+&nbsp;&nbsp;&nbsp;<img src="http://balancer.ru/_bors/i/thumb_<?=$x->score() > 0 ? 'up' : 'down'?>.gif" />&nbsp;<?=$x->target()->titled_url()?> <?=$x->target()->score_colorized()?><br/>
+<?php endif ?>
+</dd>
+</dl>
+</td><td>
+
+<dl class="box">
+<dt>Лучшие <?=count($best)?> сообщений форума за всю историю</dt>
 <dd>
 <?php foreach($best as $x) if($x && $x->target()):?>
 &nbsp;&nbsp;&nbsp;<img src="http://balancer.ru/_bors/i/thumb_<?=$x->score() > 0 ? 'up' : 'down'?>.gif" />&nbsp;<?=$x->target()->titled_url()?> <?=$x->target()->score_colorized()?><br/>
 <?php endif ?>
+</dd>
+</dl>
+
+</td></tr></table>
+
+<dl class="box">
+<dt>Самые неоднозначные <?=count($differents)?> сообщений за месяц</dt>
+<dd>
+<?php foreach($differents as $x):?>
+&nbsp;&nbsp;&nbsp;<img src="http://balancer.ru/_bors/i/thumb_<?=$x->score() > 0 ? 'up' : 'down'?>.gif" />&nbsp;<?=$x->target()->titled_url()?> <?=$x->target()->score_colorized()?><br/>
+<?php endforeach ?>
 </dd>
 </dl>
 
