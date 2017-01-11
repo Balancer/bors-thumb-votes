d7588718cefd9614964d30cbb57b9bef486655d9
Balancer <balancer@balancer.ru>
2017-01-12 02:13:21 +0300

    Not need.

--- phinx.php
+++ phinx.php
@@ -2,10 +2,6 @@
 
 require __DIR__.'/../../../vendor/autoload.php';
 
-bors::init_new();
-
-require_once __DIR__.'/../../../config-host.php';
-
 return [
 	'paths' => [
 		'migrations' => __DIR__.'/migrations'
